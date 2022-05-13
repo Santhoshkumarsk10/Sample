c2e6b1c6749e7e4e686676cacc166e58f16db1f7
Santhoshkumarsk10 <bsanthoshkumar10@gmail.com>
2022-05-13 07:14:35 +0530

    repeat number series fixes

--- assignments/Day-8/repeatnumseries.sh
+++ assignments/Day-8/repeatnumseries.sh
@@ -1,20 +1,16 @@
 #!/bin/bash -x
 
-for((i=1; i<=100; i++))
+n=0
+for((n=1; n<100; i++))
 do
-	if (($i<=100))
-	then
-		case $i in
-			11) echo $i;;
-			22) echo $i;;
-			33) echo $i;;
-			44) echo $i;;
-			55) echo $i;;
-			66) echo $i;;
-			77) echo $i;;
-			88) echo $i;;
-			99) echo $i;;
-			*) echo "";;
-		esac
-	fi
+        while (($n<100))
+        do
+                $((n++))
+                mod=$((n%10))
+                a=$((n/10))
+                if (( $a == $mod ))
+                then
+                        echo $n
+                fi
+        done
 done
